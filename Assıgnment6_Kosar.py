32ef1cb62ef4d6b93f36dbecf1178e825f393b08
behry <46944445+behry@users.noreply.github.com>
2020-10-19 23:29:23 +0200

    Add files via upload

---
+++ Assıgnment6_Kosar.py
@@ -0,0 +1,14 @@
+#Exercise_1
+Week = ('Monday', 'Tuesday', 'Wednesday', 'Thursday','Friday','Saturday','Sunday')
+
+#Exercise_2
+fruits = set(['apple', 'mango', 'orange'])
+
+#Exercise_3
+new_fruits = {'chery', 'peach','apple', 'mango'}
+
+#Exercise_4
+print(new_fruits.difference(fruits))
+
+#Exercise_5
+print(new_fruits.intersection(fruits))
